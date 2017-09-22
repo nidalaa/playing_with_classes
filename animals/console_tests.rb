b297e0475dc2aa165cf820d4b075ac0d4ff7a3fa
Emilia Andrzejewska <alime1722@gmail.com>
2017-09-22 15:30:20 +0200

    [#1] class definition

---
+++ animals/console_tests.rb
@@ -0,0 +1,10 @@
+require "./animals/animal.rb"
+
+# Please require this file and try to do each of below instructions:
+
+# animal_1 = Animal.new
+# animal_2 = Animal.new
+
+
+# animal_1
+# animal_2
---
+++ lessons.md
@@ -0,0 +1,1 @@
+1. Class definition
